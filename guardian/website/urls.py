07cc0e8e273c8e395fb5e8bcec0aa9ea567ff989
rafaelcerq28 <rafaelcerq28@gmail.com>
2021-02-16 17:17:53 -0300

    criação de paginas

--- guardian/website/urls.py
+++ guardian/website/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('',views.index),
     path('login',views.login),
+    path('cadprodutos',views.cadprodutos),
 ]
--- guardian/website/views.py
+++ guardian/website/views.py
@@ -5,3 +5,6 @@ def index(request):
 
 def login(request):
     return render(request,'website/login.html')
+
+def cadprodutos(request):
+    return render(request,'website/cadprodutos.html')
